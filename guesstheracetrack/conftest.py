33a14505d33f412cf8c89008a151e0c584cabc1e
WilliamHoltsdalen <williamlh@hotmail.no>
2025-01-29 07:58:23 +0100

    Initial commit after project init

---
+++ guesstheracetrack/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from guesstheracetrack.users.models import User
+from guesstheracetrack.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
