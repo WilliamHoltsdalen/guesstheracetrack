8cad873864b9fc8c10bdc28abbaefd0a01a42313
WilliamHoltsdalen <williamlh@hotmail.no>
2025-01-30 19:35:53 +0100

    Changes to accommodate new game models

--- guesstheracetrack/games/admin.py
+++ guesstheracetrack/games/admin.py
@@ -1,1 +1,17 @@
-# Register your models here.
+from django.contrib import admin
+from django.utils.html import format_html
+
+from .models import RaceTrack
+
+
+@admin.register(RaceTrack)
+class RaceTrackAdmin(admin.ModelAdmin):
+    list_display = ("name", "difficulty", "created_at", "image_preview")
+
+    @admin.display(
+        description="Preview",
+    )
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="100px">', obj.image_url)
+        return "(No image)"
