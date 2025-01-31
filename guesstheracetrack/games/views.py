626e502af22ba39f19ed3105683dcfce1faf7578
WilliamHoltsdalen <williamlh@hotmail.no>
2025-01-31 16:32:14 +0100

    Add demo view to show racetrack image from DB

--- guesstheracetrack/games/views.py
+++ guesstheracetrack/games/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from django.shortcuts import render
+
+from .models import RaceTrack
+
+
+def home(request):
+    """Example view to test showing image from DB in template"""
+
+    track = RaceTrack.objects.get(name="nurburgring")
+
+    context = {
+        "track": track,
+    }
+
+    return render(request, "games/track.html", context)
