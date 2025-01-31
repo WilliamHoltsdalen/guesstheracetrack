da073049878e71f44a6f7287c569dc1b64ef13df
WilliamHoltsdalen <williamlh@hotmail.no>
2025-01-31 21:35:01 +0100

    Make the demo view show random track in DB

--- guesstheracetrack/games/views.py
+++ guesstheracetrack/games/views.py
@@ -1,3 +1,5 @@
+from random import choice
+
 from django.shortcuts import render
 
 from .models import RaceTrack
@@ -6,10 +8,13 @@ from .models import RaceTrack
 def home(request):
     """Example view to test showing image from DB in template"""
 
-    track = RaceTrack.objects.get(name="nurburgring")
+    # Get random track from database
+    pks = RaceTrack.objects.values_list("pk", flat=True)
+    random_pk = choice(pks)  # noqa: S311 (not for cryptographic purposes)
+    random_track = RaceTrack.objects.get(pk=random_pk)
 
     context = {
-        "track": track,
+        "track": random_track,
     }
 
     return render(request, "games/track.html", context)
