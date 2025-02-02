9eb6f5776efca68852eb0b1d5df59aae08af97ee
WilliamHoltsdalen <williamlh@hotmail.no>
2025-02-02 17:02:01 +0100

    Add view and path for 'famous_tracks' game

--- guesstheracetrack/games/urls.py
+++ guesstheracetrack/games/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from .views import famous_tracks
 from .views import home
 
 app_name = "games"
 urlpatterns = [
     path("", view=home, name="home"),
+    path("famous_tracks/", view=famous_tracks, name="famous_tracks"),
 ]
--- guesstheracetrack/games/views.py
+++ guesstheracetrack/games/views.py
@@ -6,15 +6,28 @@ from .models import RaceTrack
 
 
 def home(request):
+    return render(request, "games/home.html")
+
+
+def famous_tracks(request):
     """Example view to test showing image from DB in template"""
 
     # Get random track from database
-    pks = RaceTrack.objects.values_list("pk", flat=True)
-    random_pk = choice(pks)  # noqa: S311 (not for cryptographic purposes)
-    random_track = RaceTrack.objects.get(pk=random_pk)
+    pks = list(RaceTrack.objects.values_list("pk", flat=True))
+    random_pk_1 = choice(pks)  # noqa: S311 (not for cryptographic purposes)
+    pks.remove(random_pk_1)
+    random_pk_2 = choice(pks)  # noqa: S311 (not for cryptographic purposes)
+    pks.remove(random_pk_2)
+    random_pk_3 = choice(pks)  # noqa: S311 (not for cryptographic purposes)
+
+    correct_track = RaceTrack.objects.get(pk=random_pk_1)
+    random_track_2 = RaceTrack.objects.get(pk=random_pk_2)
+    random_track_3 = RaceTrack.objects.get(pk=random_pk_3)
 
     context = {
-        "track": random_track,
+        "correct_track": correct_track,
+        "track_2": random_track_2,
+        "track_3": random_track_3,
     }
 
-    return render(request, "games/track.html", context)
+    return render(request, "games/famous_tracks.html", context)
