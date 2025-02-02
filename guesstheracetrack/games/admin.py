84412c707b9a9486981dfdf18d6fcc4ecdd94869
WilliamHoltsdalen <williamlh@hotmail.no>
2025-02-02 22:17:19 +0100

    Register rest of 'games' models in admin.py

--- guesstheracetrack/games/admin.py
+++ guesstheracetrack/games/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.utils.html import format_html
 
+from .models import GameSession
+from .models import GameSessionTrack
+from .models import Hint
 from .models import RaceTrack
 
 
@@ -15,3 +18,35 @@ class RaceTrackAdmin(admin.ModelAdmin):
         if obj.image:
             return format_html('<img src="{}" width="100px">', obj.image.url)
         return "(No image)"
+
+
+@admin.register(GameSession)
+class GameSessionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "tracks",
+        "start_time",
+        "end_time",
+        "score",
+        "is_completed",
+    )
+    list_filter = ("user", "tracks", "is_completed")
+    search_fields = ("user__username",)
+    ordering = ("-start_time",)
+
+
+@admin.register(GameSessionTrack)
+class GameSessionTrackAdmin(admin.ModelAdmin):
+    list_display = ("session", "track", "score", "order")
+    list_filter = ("session", "track")
+    search_fields = ("session__user__username", "track__name")
+    ordering = ("session__start_time",)
+
+
+@admin.register(Hint)
+class HintAdmin(admin.ModelAdmin):
+    list_display = ("game_session", "image_segment", "revealed_at")
+    list_filter = ("game_session",)
+    search_fields = ("game_session__user__username",)
+    ordering = ("game_session__start_time",)
