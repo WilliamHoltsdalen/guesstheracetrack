01e10757505b2ad2fe80cf96bf0561af54ca385b
WilliamHoltsdalen <williamlh@hotmail.no>
2025-02-10 20:24:37 +0100

    Fix error in GH ci action migrations check

--- guesstheracetrack/games/forms.py
+++ guesstheracetrack/games/forms.py
@@ -4,8 +4,11 @@ from .models import RaceTrack
 
 
 class TrackChoiceForm(forms.Form):
-    all_pks = list(RaceTrack.objects.values_list("pk", flat=True))
+    track = forms.ChoiceField(choices=[])
 
-    track = forms.ChoiceField(
-        choices=[(pk, RaceTrack.objects.get(pk=pk).name) for pk in all_pks],
-    )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        all_pks = list(RaceTrack.objects.values_list("pk", flat=True))
+        self.fields["track"].choices = [
+            (pk, RaceTrack.objects.get(pk=pk).name) for pk in all_pks
+        ]
