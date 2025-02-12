40e76c4ef0f8178b99df006cbd7cc3508fa65097
WilliamHoltsdalen <williamlh@hotmail.no>
2025-02-12 16:33:59 +0100

    Update url's with new view names

--- guesstheracetrack/games/urls.py
+++ guesstheracetrack/games/urls.py
@@ -2,11 +2,17 @@ from django.urls import path
 
 from .views import famous_tracks
 from .views import home
-from .views import start
+from .views import session_complete
+from .views import start_session
 
 app_name = "games"
 urlpatterns = [
     path("", view=home, name="home"),
     path("famous_tracks/", view=famous_tracks, name="famous_tracks"),
-    path("famous_tracks/start/", view=start, name="famous_tracks/start"),
+    path(
+        "famous_tracks/start_session/",
+        view=start_session,
+        name="famous_tracks/start_session",
+    ),
+    path("session_complete/", view=session_complete, name="session_complete"),
 ]
