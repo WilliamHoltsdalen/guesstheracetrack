432d20074cd620456525cadb197092aa24deb5f6
WilliamHoltsdalen <williamlh@hotmail.no>
2025-02-13 23:56:37 +0100

    Update view.py to work with model changes

--- guesstheracetrack/games/views.py
+++ guesstheracetrack/games/views.py
@@ -10,36 +10,50 @@ from .models import GameSession
 from .models import GameSessionTrack
 from .models import RaceTrack
 
+MIN_TRACKS_FOR_SESSION = 3
+
 
 def home(request):
     return render(request, "games/home.html")
 
 
 def start_session(request):
-    track_list = []
-    # Get (max 10) random tracks from database, ensuring no duplicates
-    pks = list(RaceTrack.objects.values_list("pk", flat=True))
-    for _ in range(min(10, len(pks))):
-        random_pk = choice(pks)  # noqa: S311 (not for cryptographic purposes)
-        track_list.append(RaceTrack.objects.get(pk=random_pk))
-        pks.remove(random_pk)
+    # Minimum of 3 tracks required
 
-    shuffle(track_list)
+    pks = list(RaceTrack.objects.values_list("pk", flat=True)).copy()
+    unused_pks = pks.copy()
+    # If there are less than minimum of 3 tracks in the database, redirect
+    if len(pks) < MIN_TRACKS_FOR_SESSION:
+        return redirect("games:home")
 
-    # Create a new game session
+    # Create a new game session with rounds
     game_session = GameSession.objects.create(
         user=request.user,
     )
-    game_session_track_list = []
 
-    for i, track in enumerate(track_list):
-        game_session_track_list.append(
-            GameSessionTrack.objects.create(
-                session=game_session,
-                track=track,
-                order=i,
-            ),
+    # Get (max 10) random tracks from database, ensuring no duplicates
+    for i in range(min(10, len(pks) - 1)):
+        correct_track_pk = choice(unused_pks)  # noqa: S311 (not for cryptographic purposes)
+        unused_pks.remove(correct_track_pk)
+        non_correct_pks = pks.copy()
+        non_correct_pks.remove(correct_track_pk)
+
+        incorrect_track_1_pk = choice(non_correct_pks)  # noqa: S311 (not for cryptographic purposes)
+        non_correct_pks.remove(incorrect_track_1_pk)
+        incorrect_track_2_pk = choice(non_correct_pks)  # noqa: S311 (not for cryptographic purposes)
+
+        correct_track = RaceTrack.objects.get(pk=correct_track_pk)
+        incorrect_track_1 = RaceTrack.objects.get(pk=incorrect_track_1_pk)
+        incorrect_track_2 = RaceTrack.objects.get(pk=incorrect_track_2_pk)
+
+        GameSessionTrack.objects.create(
+            session=game_session,
+            correct_track=correct_track,
+            incorrect_track_1=incorrect_track_1,
+            incorrect_track_2=incorrect_track_2,
+            order=i,
         )
+
     return redirect("games:famous_tracks")
 
 
@@ -61,7 +75,7 @@ def famous_tracks(request):
             score=0,
         ).first()
 
-        if game_session_track.track.pk == track_pk:
+        if game_session_track.correct_track.pk == track_pk:
             game_session_track.score += 1
             game_session_track.save()
         else:
@@ -89,24 +103,17 @@ def famous_tracks(request):
         .order_by("-start_time")
         .first()
     )
-
     game_session_track = GameSessionTrack.objects.filter(
         session=game_session,
         score=0,
     ).first()
 
-    # Remove the correct track from the list of tracks, and get 2 random ones
-    pks = list(RaceTrack.objects.values_list("pk", flat=True))
-    pks.remove(game_session_track.track.pk)
-    random_pk_2 = choice(pks)  # noqa: S311 (not for cryptographic purposes)
-    pks.remove(random_pk_2)
-    random_pk_3 = choice(pks)  # noqa: S311 (not for cryptographic purposes)
-
-    correct_track = game_session_track.track
-    random_track_2 = RaceTrack.objects.get(pk=random_pk_2)
-    random_track_3 = RaceTrack.objects.get(pk=random_pk_3)
-
-    track_list = [correct_track, random_track_2, random_track_3]
+    # Get the track choices for the current round and shuffle them
+    track_list = [
+        game_session_track.correct_track,
+        game_session_track.incorrect_track_1,
+        game_session_track.incorrect_track_2,
+    ]
     shuffle(track_list)
 
     # Get status of game session
@@ -122,11 +129,12 @@ def famous_tracks(request):
             .score
         )
 
+    # Get round number for the current round
     current_round = game_session_track.order + 1
 
     context = {
         "track_list": track_list,
-        "correct_track_pk": correct_track.pk,
+        "correct_track_pk": game_session_track.correct_track.pk,
         "rounds": rounds,
         "current_round": current_round,
         "number_of_rounds": number_of_rounds,
@@ -156,7 +164,7 @@ def session_complete(request):
                 order=track_round,
             )
             .first()
-            .track.name,
+            .correct_track.name,
             "score": GameSessionTrack.objects.filter(
                 session=game_session,
                 order=track_round,
