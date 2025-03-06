ed26048e481b8fd86efe2ec5b785d74b56e81f52
WilliamHoltsdalen <williamlh@hotmail.no>
2025-03-06 22:38:36 +0100

    refactor(util): Make the image_segments be saved to media dir

--- guesstheracetrack/games/utils.py
+++ guesstheracetrack/games/utils.py
@@ -1,6 +1,7 @@
 import uuid
 from pathlib import Path
 
+from django.conf import settings
 from django.utils.deconstruct import deconstructible
 from PIL import Image
 
@@ -19,14 +20,14 @@ class RandomFileName:
 @deconstructible
 class SegmentImage:
     def __init__(self, image_path, segment_count):
-        self.image_path = image_path
+        self.full_image_path = "/app/guesstheracetrack" + image_path
         self.segment_count = segment_count
 
     def __call__(self):
         return self.segment_image()
 
     def segment_image(self):
-        image = Image.open(self.image_path)
+        image = Image.open(self.full_image_path)
         width, height = image.size
         segment_width = width // self.segment_count
         segment_height = height // self.segment_count
@@ -39,6 +40,13 @@ class SegmentImage:
                 right = left + segment_width
                 lower = upper + segment_height
                 segment = image.crop((left, upper, right, lower))
-                segments_list.append(segment)
+
+                # Save the segment to MEDIA_ROOT
+                segment_filename = f"segments/segment_{i}_{j}.png"
+                segment_path = Path(settings.URL) / segment_filename
+                segment_full_path = Path(settings.MEDIA_ROOT) / segment_filename
+                segment.save(segment_full_path)
+
+                segments_list.append(segment_path)
 
         return segments_list
