55407f36f6a660dc70cc6a88085c0171a33a503e
WilliamHoltsdalen <williamlh@hotmail.no>
2025-03-06 14:27:46 +0100

    feat(util): Add image segmentation function

--- guesstheracetrack/games/utils.py
+++ guesstheracetrack/games/utils.py
@@ -2,6 +2,7 @@ import uuid
 from pathlib import Path
 
 from django.utils.deconstruct import deconstructible
+from PIL import Image
 
 
 @deconstructible
@@ -13,3 +14,22 @@ class RandomFileName:
         extension = Path(filename).suffix
         new_filename = f"{uuid.uuid4()}{extension}"
         return self.path / new_filename
+
+
+def segment_image(image_path, segments):
+    image = Image.open(image_path)
+    width, height = image.size
+    segment_width = width // segments
+    segment_height = height // segments
+    segments_list = []
+
+    for i in range(segments):
+        for j in range(segments):
+            left = i * segment_width
+            upper = j * segment_height
+            right = left + segment_width
+            lower = upper + segment_height
+            segment = image.crop((left, upper, right, lower))
+            segments_list.append(segment)
+
+    return segments_list
