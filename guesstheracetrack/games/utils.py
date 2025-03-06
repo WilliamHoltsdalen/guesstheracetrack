1097efe5a9c0b55529299e64215f083869603a8d
WilliamHoltsdalen <williamlh@hotmail.no>
2025-03-06 14:36:48 +0100

    refactor(util): Refactor utils.py

--- guesstheracetrack/games/utils.py
+++ guesstheracetrack/games/utils.py
@@ -16,20 +16,29 @@ class RandomFileName:
         return self.path / new_filename
 
 
-def segment_image(image_path, segments):
-    image = Image.open(image_path)
-    width, height = image.size
-    segment_width = width // segments
-    segment_height = height // segments
-    segments_list = []
+@deconstructible
+class SegmentImage:
+    def __init__(self, image_path, segment_count):
+        self.image_path = image_path
+        self.segment_count = segment_count
 
-    for i in range(segments):
-        for j in range(segments):
-            left = i * segment_width
-            upper = j * segment_height
-            right = left + segment_width
-            lower = upper + segment_height
-            segment = image.crop((left, upper, right, lower))
-            segments_list.append(segment)
+    def __call__(self):
+        return self.segment_image()
 
-    return segments_list
+    def segment_image(self):
+        image = Image.open(self.image_path)
+        width, height = image.size
+        segment_width = width // self.segment_count
+        segment_height = height // self.segment_count
+        segments_list = []
+
+        for i in range(self.segment_count):
+            for j in range(self.segment_count):
+                left = i * segment_width
+                upper = j * segment_height
+                right = left + segment_width
+                lower = upper + segment_height
+                segment = image.crop((left, upper, right, lower))
+                segments_list.append(segment)
+
+        return segments_list
