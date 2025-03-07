38e55fe6f9e1951810280d9c313eb5397511bf9b
WilliamHoltsdalen <williamlh@hotmail.no>
2025-03-07 12:05:39 +0100

    feat(util): Update SegmentImage to use hashed filenames, ++

--- guesstheracetrack/games/utils.py
+++ guesstheracetrack/games/utils.py
@@ -1,3 +1,5 @@
+import hashlib
+import os
 import uuid
 from pathlib import Path
 
@@ -18,10 +20,22 @@ class RandomFileName:
 
 
 @deconstructible
+class HashedFileName:
+    def __init__(self, path):
+        self.path = Path(path)
+
+    def __call__(self, _, filename):
+        extension = Path(filename).suffix
+        new_filename = f"{hashlib.sha256(filename.encode()).hexdigest()}{extension}"
+        return self.path / new_filename
+
+
+@deconstructible
 class SegmentImage:
-    def __init__(self, image_path, segment_count):
+    def __init__(self, image_path, count_sqroot):
         self.full_image_path = "/app/guesstheracetrack" + image_path
-        self.segment_count = segment_count
+        self.image_path = image_path
+        self.count_sq_root = count_sqroot
 
     def __call__(self):
         return self.segment_image()
@@ -29,24 +43,35 @@ class SegmentImage:
     def segment_image(self):
         image = Image.open(self.full_image_path)
         width, height = image.size
-        segment_width = width // self.segment_count
-        segment_height = height // self.segment_count
+        segment_width = width // self.count_sq_root
+        segment_height = height // self.count_sq_root
         segments_list = []
 
-        for i in range(self.segment_count):
-            for j in range(self.segment_count):
+        for i in range(self.count_sq_root):
+            for j in range(self.count_sq_root):
                 left = i * segment_width
                 upper = j * segment_height
                 right = left + segment_width
                 lower = upper + segment_height
                 segment = image.crop((left, upper, right, lower))
 
-                # Save the segment to MEDIA_ROOT
-                segment_filename = f"segments/segment_{i}_{j}.png"
-                segment_path = Path(settings.URL) / segment_filename
-                segment_full_path = Path(settings.MEDIA_ROOT) / segment_filename
+                hashed_file_name = HashedFileName("segments")
+                segment_filename = hashed_file_name(
+                    None,
+                    Path(self.image_path).name
+                    + f"{i}_{j}"
+                    + Path(self.image_path).suffix,
+                )
+                segment_path = os.path.join(settings.MEDIA_URL, segment_filename)  # noqa: PTH118 use of os.path.join is fine here
+                segment_full_path = os.path.join(settings.MEDIA_ROOT, segment_filename)  # noqa: PTH118 use of os.path.join is fine here
                 segment.save(segment_full_path)
 
-                segments_list.append(segment_path)
+                segments_list.append(
+                    {
+                        "i": i + 1,
+                        "j": j + 1,
+                        "segment": segment_path,
+                    },
+                )
 
         return segments_list
