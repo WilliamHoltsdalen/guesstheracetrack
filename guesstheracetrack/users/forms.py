7170c54f683d2b7a48f02b88e1e481587f8033da
WilliamHoltsdalen <williamlh@hotmail.no>
2025-07-02 21:30:17 +0200

    fix(forms): Add missing import for TextInput

--- guesstheracetrack/users/forms.py
+++ guesstheracetrack/users/forms.py
@@ -3,6 +3,7 @@ from allauth.socialaccount.forms import SignupForm as SocialSignupForm
 from django.contrib.auth import forms as admin_forms
 from django.forms import CharField
 from django.forms import EmailField
+from django.forms import TextInput
 from django.utils.translation import gettext_lazy as _
 
 from .models import User
