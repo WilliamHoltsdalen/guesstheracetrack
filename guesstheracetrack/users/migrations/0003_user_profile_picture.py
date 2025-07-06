e39b266173bb7f3822b335eb118f59cfa4d11309
WilliamHoltsdalen <williamlh@hotmail.no>
2025-07-06 21:31:41 +0200

    refactor(migration): Change default profile pic file name

--- guesstheracetrack/users/migrations/0003_user_profile_picture.py
+++ guesstheracetrack/users/migrations/0003_user_profile_picture.py
@@ -6,13 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0002_alter_user_name'),
+        ("users", "0002_alter_user_name"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='profile_picture',
-            field=models.ImageField(blank=True, default='profile_pics/default.png', null=True, upload_to='profile_pics/'),
+            model_name="user",
+            name="profile_picture",
+            field=models.ImageField(
+                blank=True,
+                default="profile_pics/profile_picture.jpg",
+                null=True,
+                upload_to="profile_pics/",
+            ),
         ),
     ]
