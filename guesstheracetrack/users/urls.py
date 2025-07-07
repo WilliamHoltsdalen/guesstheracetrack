38319d8fd5b5ba26d1053513eb6d1c3df373520d
WilliamHoltsdalen <williamlh@hotmail.no>
2025-07-07 14:37:26 +0200

    feat(view): Add url and view for game_history

--- guesstheracetrack/users/urls.py
+++ guesstheracetrack/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import UserDeleteView
+from .views import game_history_view
 from .views import reset_progress_view
 from .views import user_detail_view
 from .views import user_redirect_view
@@ -12,5 +13,6 @@ urlpatterns = [
     path("~update/", view=user_update_view, name="update"),
     path("~delete/", UserDeleteView.as_view(), name="delete"),
     path("~reset-progress/", reset_progress_view, name="reset_progress"),
+    path("game_history/", view=game_history_view, name="game_history"),
     path("<str:pk>/", view=user_detail_view, name="detail"),
 ]
